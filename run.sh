72a5a28e0e4a60da4bb26e4ab9e5ebc5a2b712f9
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-04 21:39:22 +0900

    Change Link Directory

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ docker rm hoppin
 docker build --no-cache --rm -t test:0.1.0 .
 
 # リンクして実行
-docker run -d -p 443:8080 --name hoppin --link mysql:mysql test:0.1.0
+docker run -d -p 443:8080 -v $(cd $(dirname $0) && pwd)/ssl:/ssl:ro --name hoppin --link mysql:mysql test:0.1.0
