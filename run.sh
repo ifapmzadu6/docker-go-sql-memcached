c3bd387de5cc7f9070779e6456c5f278c79e336e
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-05-10 00:53:15 +0900

    ビルドの順番を変更

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 # このディレクトリに移動
 cd `dirname $0`
 
-# 前回起動中のものがあれば削除
-docker stop hoppin
-docker rm hoppin
+# Delete all containers
+docker rm $(docker ps -a -q)
+# Delete all images
+docker rmi $(docker images -q)
 
 # ビルド
 docker build --no-cache --rm -t test:0.1.0 .
+
+# 前回起動中のものがあれば削除
+docker stop hoppin
+docker rm hoppin
 
 # リンクして実行
 docker run -d -p 443:8080 -v $(pwd)/ssl:/ssl:ro --name hoppin --link mysql:mysql test:0.1.0
